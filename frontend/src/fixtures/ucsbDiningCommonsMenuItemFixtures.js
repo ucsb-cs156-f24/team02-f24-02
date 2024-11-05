ed22c4a7662c0eb6ab5149635970276f94c8140e
leozhu0 <leozhu@ucsb.edu>
2024-11-05 13:41:48 -0800

    Created ucsbDiningCommonsMenuItemFixtures.js

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,31 @@
+const ucsbDiningCommonsMenuItemsFixtures = {
+    oneDiningCommonsMenuItem: {
+      id: 1,
+      diningCommonsCode: "Portola",
+      name: "Pasta",
+      station: "Brick",
+    },
+    threeDiningCommonsMenuItems: [
+      {
+        id: 1,
+        diningCommonsCode: "Portola",
+        name: "Pasta",
+        station: "Brick",
+      },
+      {
+        id: 2,
+        diningCommonsCode: "Portola",
+        name: "Pizza",
+        station: "Brick",
+      },
+      {
+        id: 3,
+        diningCommonsCode: "Portola",
+        name: "Salad",
+        station: "Brick",
+      },
+    ],
+  };
+  
+  export { ucsbDiningCommonsMenuItemsFixtures };
+  
