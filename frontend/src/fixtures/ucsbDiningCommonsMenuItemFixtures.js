ad5deaf04ce5046f6f9ce934fe32d95928aff632
leozhu0 <leozhu@ucsb.edu>
2024-11-05 17:35:26 -0800

    fixed formatting

--- frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -1,31 +1,30 @@
 const ucsbDiningCommonsMenuItemsFixtures = {
-    oneDiningCommonsMenuItem: {
+  oneDiningCommonsMenuItem: {
+    id: 1,
+    diningCommonsCode: "Portola",
+    name: "Pasta",
+    station: "Brick",
+  },
+  threeDiningCommonsMenuItems: [
+    {
       id: 1,
       diningCommonsCode: "Portola",
       name: "Pasta",
       station: "Brick",
     },
-    threeDiningCommonsMenuItems: [
-      {
-        id: 1,
-        diningCommonsCode: "Portola",
-        name: "Pasta",
-        station: "Brick",
-      },
-      {
-        id: 2,
-        diningCommonsCode: "Portola",
-        name: "Pizza",
-        station: "Brick",
-      },
-      {
-        id: 3,
-        diningCommonsCode: "Portola",
-        name: "Salad",
-        station: "Brick",
-      },
-    ],
-  };
-  
-  export { ucsbDiningCommonsMenuItemsFixtures };
-  
+    {
+      id: 2,
+      diningCommonsCode: "Portola",
+      name: "Pizza",
+      station: "Brick",
+    },
+    {
+      id: 3,
+      diningCommonsCode: "Portola",
+      name: "Salad",
+      station: "Brick",
+    },
+  ],
+};
+
+export { ucsbDiningCommonsMenuItemsFixtures };
