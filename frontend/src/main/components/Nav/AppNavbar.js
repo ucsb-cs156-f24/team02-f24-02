e91ca520860a1408881214308991afb575546590
leozhu0 <leozhu@ucsb.edu>
2024-11-08 11:28:25 -0800

    changed navbar text

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -64,7 +64,7 @@ export default function AppNavbar({
                     Restaurants
                   </Nav.Link>
                   <Nav.Link as={Link} to="/ucsbdiningcommonsmenuitems">
-                    UCSB Dining Commons Menu Items
+                    Menu Items
                   </Nav.Link>
                   <Nav.Link as={Link} to="/ucsbdates">
                     UCSB Dates
