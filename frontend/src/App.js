dcc75fba8a46a606136672d3d76b6d91638bdc13
Edwin Medrano <eduino@100-64-48-56.wireless.ucsb.edu>
2024-11-11 13:48:26 -0800

    EMV - THIS MIGHT BE IT

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -49,7 +49,7 @@ function App() {
           <>
             <Route
               exact
-              path="/ucsborganizations/edit/:id"
+              path="/ucsborganizations/edit/:orgCode"
               element={<UCSBOrganizationEditPage />}
             />
             <Route
